482a6337b61c4db7707189f592e9ae3dd8676d87
wy@manaosoftware.com <wy@manaosoftware.com>
2024-10-03 09:25:05 +0700

    init

---
+++ hosting/src/app/(pages)/about/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Head from 'next/head';
+
+
+export default function About() {
+    return (
+        <>
+            <Head>
+                <title>About Us 5</title>
+                <meta name="description" content="Learn more about our company and team." />
+            </Head>
+            <main>
+                <h1>About Us</h1>
+                <p>Welcome to our company. We are dedicated to providing the best service possible.</p>
+            </main>
+        </>
+    );
+};
